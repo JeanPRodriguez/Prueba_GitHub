cf94f2b7f3161db7d89ae859d4a428da92e8ce03
JeanPRodriguez <51722221+JeanPRodriguez@users.noreply.github.com>
2019-07-01 11:27:40 -0500

    Ira pues

---
+++ Inicio/Ejercicios con Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_con_Arrays
+{
+    class Program
+    {
+        public int k = 0, sumapos = 0;
+
+        public string EliminarEspacios(string nombre, int[] posicion)
+        {
+
+            foreach (var i in nombre)
+            {
+                k += 1;
+                if (i == ' ')
+                {
+                    int j = 0;
+                    posicion[j] = k;
+                    j++;
+                }
+                
+            }
+
+            for (int j = 0; j > posicion.Length; j++)
+            {
+               sumapos = posicion[j];
+            }
+
+            return sumapos.ToString();
+        }
+
+    
+        static void Main(string[] args)
+        {
+          
+             int[] lista = { 1, 2, 3, 4, 5, 6, 7 };
+            string name = "VOY AL BAÑO Y YA VUELVO";
+            int[] Posición = new int[name.Length];
+            
+
+            EliminarEspacios(name, Posición[]);
+
+        }
+        }
+    }
+
